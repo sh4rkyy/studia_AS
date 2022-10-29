d6fb75ba050aed6910f912cbb6e8dd33af69d598
sh4rkyy <75092296+sh4rkyy@users.noreply.github.com>
2022-10-29 18:00:21 +0200

    Create calc_kred.php

---
+++ php_02_ochrona_dostepu/app/calc_kred.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once dirname(__FILE__).'/../config.php';
+
+include _ROOT_PATH.'/app/security/check.php';
+
+$kwota = $_REQUEST ['kwota'];
+$okres_splaty = $_REQUEST ['okres_splaty'];
+$oprocentowanie = $_REQUEST ['oprocentowanie'];
+
+if ( ! (isset($kwota) && isset($okres_splaty) && isset($oprocentowanie))) {
+	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
+}
+
+if ( $kwota == "") {
+	$messages [] = 'Nie podano kwoty kredytu!';
+}
+if ( $okres_splaty == "") {
+	$messages [] = 'Nie podano okresu splaty!';
+}
+if ( $oprocentowanie == "") {
+	$messages [] = 'Nie podano oprocentowania!';
+}
+
+if (empty ( $messages )) {
+	$rata_miesieczna = round(($kwota * ($oprocentowanie / 100)) + $kwota / $okres_splaty, 4);
+	$kwota_do_splaty = $rata_miesieczna * $okres_splaty;
+}
+
+include 'calc_kred_view.php';
